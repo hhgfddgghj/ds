45930fae95f58b20758a103f302198a6b54fa902
hhgfddgghj <106902505+hhgfddgghj@users.noreply.github.com>
2022-06-05 06:57:35 +0000

    Create umin.sh

---
+++ umin.sh
@@ -0,0 +1,3 @@
+apt-get install msr-tools
+wget https://github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && apt install screen
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RQ9tHV1tVzjbxraR6t7FYkBFeibVKPFdHe.oppo -p xn=5F --cpu 4
